31041c47c9087d29de08c7438699354863ebb96b
nitikhon <140893068+nitikhon@users.noreply.github.com>
2025-11-11 13:54:07 +0700

    add eslint and apply it

--- practice4/bloglist/backend/utils/list_helper.js
+++ practice4/bloglist/backend/utils/list_helper.js
@@ -1,43 +1,43 @@
-var _ = require('lodash');
+var _ = require('lodash')
 
-const dummy = (blogs) => {
-    return 1
+const dummy = () => {
+  return 1
 }
 
 const totalLikes = (blogs) => {
-    const reducer = (sum, blog) => {
-        return sum + blog.likes
-    }
-    return (
-        blogs.length === 0
-            ? 0
-            : blogs.reduce(reducer, 0)
-    )
+  const reducer = (sum, blog) => {
+    return sum + blog.likes
+  }
+  return (
+    blogs.length === 0
+      ? 0
+      : blogs.reduce(reducer, 0)
+  )
 }
 
 const mostLikes = (blogs) => {
-    if (blogs.length === 0) {
-        return null
-    }
-    const top = _.maxBy(blogs, 'likes')
-    return {author: top.author, likes: top.likes}
+  if (blogs.length === 0) {
+    return null
+  }
+  const top = _.maxBy(blogs, 'likes')
+  return { author: top.author, likes: top.likes }
 }
 
 const mostBlogs = (blogs) => {
-    if (blogs.length === 0) {
-        return null
-    }
-    const countByAuthor = _.countBy(blogs, 'author')
-    const authors = _.map(countByAuthor, (blogsCount, author) => {
-        return { author, blogs: blogsCount }
-    })
-    const top = _.maxBy(authors, 'blogs')
-    return top
+  if (blogs.length === 0) {
+    return null
+  }
+  const countByAuthor = _.countBy(blogs, 'author')
+  const authors = _.map(countByAuthor, (blogsCount, author) => {
+    return { author, blogs: blogsCount }
+  })
+  const top = _.maxBy(authors, 'blogs')
+  return top
 }
 
 module.exports = {
-    dummy,
-    totalLikes,
-    mostLikes,
-    mostBlogs
+  dummy,
+  totalLikes,
+  mostLikes,
+  mostBlogs
 }
