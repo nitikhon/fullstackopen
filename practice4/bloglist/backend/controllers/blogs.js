2a87c3506c14e161f4d9aa59351dc5edff1e6c41
nitikhon <140893068+nitikhon@users.noreply.github.com>
2025-11-13 17:27:01 +0700

    submit exercises 4.13 - 4.14

--- practice4/bloglist/backend/controllers/blogs.js
+++ practice4/bloglist/backend/controllers/blogs.js
@@ -17,4 +17,19 @@ blogRouter.post('/', async (request, response) => {
   response.status(201).json(result)
 })
 
+blogRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+blogRouter.put('/:id', async (request, response) => {
+  const blog = request.body
+  const result = await Blog.findByIdAndUpdate(
+    request.params.id,
+    blog,
+    { new: true, runValidators: true }
+  )
+  response.status(200).json(result)
+})
+
 module.exports = blogRouter
--- practice4/bloglist/backend/tests/blogs_api.test.js
+++ practice4/bloglist/backend/tests/blogs_api.test.js
@@ -8,93 +8,121 @@ const helper = require('./test_helper')
 
 const api = supertest(app)
 
-beforeEach(async () => {
-  await Blog.deleteMany({})
-  for (let blog of helper.initialBlogs) {
-    let newBlog = new Blog(blog)
-    await newBlog.save()
-  }
-})
-
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
-
-test('blogs are returned same amount as initialBlogs', async () => {
-  const blogs = await api.get('/api/blogs')
-
-  assert.strictEqual(blogs.body.length, helper.initialBlogs.length)
-})
-
-test('the unique identifier property of the blog posts is named id', async () => {
-  const blogs = await helper.blogsInDb()
-
-  assert.notStrictEqual(blogs[0].id, undefined)
-  assert.strictEqual(blogs[0]._id, undefined)
-})
-
-test('successfully creates a new blog post', async () => {
-  const blog = {
-    title: 'single data test',
-    author: 'juan gonsalez',
-    url: 'tacos.com',
-    likes: 11,
-  }
-  const blogsBefore = await helper.blogsInDb()
-
-  await api
-    .post('/api/blogs')
-    .send(blog)
-    .expect(201)
-
-  const blogsAfter = await helper.blogsInDb()
-
-  assert.strictEqual(blogsAfter.length, blogsBefore.length + 1)
-})
-
-describe('missing properties', () => {
-  test('like: default should be 0', async () => {
-    const newBlog = {
-      title: 'single data test',
-      author: 'juan gonsalez',
-      url: 'tacos.com',
+describe('when there is initially some blogs saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    for (let blog of helper.initialBlogs) {
+      let newBlog = new Blog(blog)
+      await newBlog.save()
     }
+  })
 
-    const response = await api.post('/api/blogs').send(newBlog)
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
+  test('blogs are returned same amount as initialBlogs', async () => {
+    const blogs = await api.get('/api/blogs')
+
+    assert.strictEqual(blogs.body.length, helper.initialBlogs.length)
+  })
+
+  test('the unique identifier property of the blog posts is named id', async () => {
     const blogs = await helper.blogsInDb()
-    const blog = blogs.find(blog => blog.id === response.body.id)
 
-    assert.strictEqual(blog.likes, 0)
+    assert.notStrictEqual(blogs[0].id, undefined)
+    assert.strictEqual(blogs[0]._id, undefined)
   })
 
-  test('title: responds with the status code 400.', async () => {
-    const newBlog = {
-      author: 'juan gonsalez',
-      url: 'tacos.com',
-      likes: 1,
-    }
+  describe('create a new blog post', () => {
+    test('successfully creates a new blog post', async () => {
+      const blog = {
+        title: 'single data test',
+        author: 'juan gonsalez',
+        url: 'tacos.com',
+        likes: 11,
+      }
+      const blogsBefore = await helper.blogsInDb()
 
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(400)
+      await api
+        .post('/api/blogs')
+        .send(blog)
+        .expect(201)
+
+      const blogsAfter = await helper.blogsInDb()
+
+      assert.strictEqual(blogsAfter.length, blogsBefore.length + 1)
+    })
+
+    describe('missing properties', () => {
+      test('like: default should be 0', async () => {
+        const newBlog = {
+          title: 'single data test',
+          author: 'juan gonsalez',
+          url: 'tacos.com',
+        }
+
+        const response = await api.post('/api/blogs').send(newBlog)
+
+        const blogs = await helper.blogsInDb()
+        const blog = blogs.find(blog => blog.id === response.body.id)
+
+        assert.strictEqual(blog.likes, 0)
+      })
+
+      test('title: responds with the status code 400.', async () => {
+        const newBlog = {
+          author: 'juan gonsalez',
+          url: 'tacos.com',
+          likes: 1,
+        }
+
+        await api
+          .post('/api/blogs')
+          .send(newBlog)
+          .expect(400)
+      })
+
+      test('url: responds with the status code 400.', async () => {
+        const newBlog = {
+          title: 'single data test',
+          author: 'juan gonsalez',
+          likes: 1
+        }
+
+        await api
+          .post('/api/blogs')
+          .send(newBlog)
+          .expect(400)
+      })
+    })
   })
 
-  test('url: responds with the status code 400.', async () => {
-    const newBlog = {
-      title: 'single data test',
-      author: 'juan gonsalez',
-      likes: 1
-    }
+  describe('delete a blog post', () => {
+    test('successfully delete blog', async () => {
+      const blogs = await helper.blogsInDb()
+      const id = blogs[0].id
+      await api
+        .delete(`/api/blogs/${id}`)
+        .expect(204)
+    })
+  })
 
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(400)
+  describe('update a blog post', () => {
+    test('successfully update blog', async () => {
+      const blogs = await helper.blogsInDb()
+      const blog = blogs[0]
+      blog.likes += 10
+      const result = await api
+        .put(`/api/blogs/${blog.id}`)
+        .send(blog)
+        .expect(200)
+
+      assert.deepStrictEqual(result.body, blog)
+    })
   })
 })
 
