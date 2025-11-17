9115e523d6a847ea4a0cb991db1a36f501d8f9a5
nitikhon <140893068+nitikhon@users.noreply.github.com>
2025-11-17 16:18:29 +0700

    fix missing blog form labels

--- practice4/bloglist/frontend/src/components/CreateBlogForm.jsx
+++ practice4/bloglist/frontend/src/components/CreateBlogForm.jsx
@@ -5,15 +5,15 @@ const CreateBlogFrom = (props) => {
             <h2>create new</h2>
             <form onSubmit={props.handleCreateBlog}>
                 <div>
-                    <label htmlFor="title"></label>
+                    <label htmlFor="title">title</label>
                     <input id="title" name="title" value={title} type="text" onChange={props.handleChange} />
                 </div>
                 <div>
-                    <label htmlFor="author"></label>
+                    <label htmlFor="author">author</label>
                     <input id="author" name="author" value={author} type="text" onChange={props.handleChange} />
                 </div>
                 <div>
-                    <label htmlFor="url"></label>
+                    <label htmlFor="url">url</label>
                     <input id="url" name="url" value={url} type="text" onChange={props.handleChange} />
                 </div>
                 <button type="submit">create</button>
